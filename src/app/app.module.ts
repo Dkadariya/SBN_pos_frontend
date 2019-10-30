e1044934370bd26a8b0b0573d73645948c3f828c
Dkadariya <dipeshk2015@gmail.com>
2019-10-30 13:44:23 -0400

    inventory mgmt component and service defination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InventMgmtComponent } from './invent-mgmt/invent-mgmt.component';
+import { Routes, RouterModule } from '@angular/router';
 
+const appRoutes: Routes = [
+  {path: 'inventory', component: InventMgmtComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventMgmtComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/invent-mgmt/invent-mgmt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invent-mgmt',
+  templateUrl: './invent-mgmt.component.html',
+  styleUrls: ['./invent-mgmt.component.css']
+})
+export class InventMgmtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
