b82666bfab1efecfb7361950a02541c8ef921caf
Dkadariya <dipeshk2015@gmail.com>
2019-11-01 00:36:51 -0400

    more UI updates including google fonts

--- src/app/invent-mgmt/invent-mgmt.component.ts
+++ src/app/invent-mgmt/invent-mgmt.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { InventoryService } from '../services/inventory.service';
 import * as moment from 'moment';
 
@@ -8,20 +8,24 @@ import * as moment from 'moment';
   styleUrls: ['./invent-mgmt.component.css']
 })
 export class InventMgmtComponent implements OnInit {
-  @ViewChild('myForm',{static: false}) formValues;
+  // getting the form element as a child of the view
+  @ViewChild('myForm', { static: false }) formValues;
   item = {};
-
+  //  injecting class dependency through constructor
   constructor(private invent: InventoryService) { }
 
   ngOnInit() {
-    
+
   }
-  
+  // function to pack the item details and send to server through service method
   addItem() {
+    // assigning randomly generated ID to Items
     this.item["id"] = Math.floor(Math.random() * 100001) + 1000;
+    // assign current timestamp as creation timestamp
     this.item["created"] = moment().format("MM-DD-YYYY HH:mm");
-    console.log(this.item);
-    this.invent.addItem (this.item).subscribe(data => {console.log(data); 
+
+    // pass the JSON packet to service addItem method
+    this.invent.addItem(this.item).subscribe(data => {
       this.formValues.resetForm();
     });
   }
--- src/app/list-invent/list-invent.component.ts
+++ src/app/list-invent/list-invent.component.ts
@@ -7,11 +7,14 @@ import { InventoryService } from '../services/inventory.service';
   styleUrls: ['./list-invent.component.css']
 })
 export class ListInventComponent implements OnInit {
+  // class variable initialization
   sell_items = [];
   total = 0;
+  order_no=0;
   items: any = [];
+  err_msg='';
 
-  // injecting the service  dependency into class through constructor
+  // injecting the service dependency into class through constructor
   constructor(private invent: InventoryService) { }
 
   // component OnInit lifecycle hook
@@ -19,16 +22,34 @@ export class ListInventComponent implements OnInit {
     // load the item list during the initialization of the component
     this.invent.list_all().subscribe(data => {
       this.items = data;
+      console.log(data);
     });
   }
 
   // function to add item to sell list
   appendItem(id, item, price) {
     let new_entry = true;
-    // if item already in the list, just increament its quantity value
-    this.sell_items.forEach(element => {
+    let available;
+    // get total available quantity of the particular item
+    this.items.forEach(element => {
       if (element.id === id) {
+        available = element.total_count;
+      }
+    });
+    // if new order started, generate a new order no.
+    if(this.order_no===0){
+      this.rand_order();
+    }
+    // if item already in the list and quantity is available, increament its quantity value
+    this.sell_items.forEach(element => {
+      if (element.id === id && element.quantity < available) {
         element.quantity += 1;
+        this.total += price;
+        new_entry = false;
+      }
+      else if(element.id === id && element.quantity >= available) {
+        this.err_msg="No more quantity available for this item";
+        setTimeout(() => { this.err_msg=''; }, 2300);
         new_entry = false;
       }
     });
@@ -36,13 +57,15 @@ export class ListInventComponent implements OnInit {
     // else if item is new in the list, add the item entry to the list
     if (new_entry) {
       this.sell_items.push({ "id": id, "item": item, "price": price, "quantity": 1 });
+      this.total += price;
     }
-    this.total += price;
   }
 
   //  function to clear the sell item list if order is cancelled
   cancelOrder() {
     this.sell_items.splice(0, this.sell_items.length);
+    this.total=0;
+    this.order_no=0;
   }
 
   //  function to update the item in the sercer database after the sell.
@@ -50,6 +73,8 @@ export class ListInventComponent implements OnInit {
     console.log(this.sell_items);
     this.invent.updateItems(this.sell_items).subscribe();
     this.sell_items.splice(0, this.sell_items.length);
+    this.total=0;
+    this.order_no=0;
 
     setTimeout(() => { this.refresh(); }, 100);
   }
@@ -77,6 +102,10 @@ export class ListInventComponent implements OnInit {
         element.quantity += 1;
         this.total += element.price;
       }
+      else{
+        this.err_msg="No more quantity available for this item";
+        setTimeout(() => { this.err_msg=''; }, 2300);
+      }
     });
   }
 
@@ -94,5 +123,9 @@ export class ListInventComponent implements OnInit {
         this.sell_items.splice(i, 1);
       }
     });
+  }
+  // function to random generate order number 
+  rand_order(){
+ this.order_no=Math.floor(Math.random() * 100001) + 1000;
   }
 }
