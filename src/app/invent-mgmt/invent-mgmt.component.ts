a015a1e81f0e956c0dc598fc4c59ac8cf18daddb
Dkadariya <dipeshk2015@gmail.com>
2019-10-31 21:18:22 -0400

    UI updates and add Item form control

--- src/app/invent-mgmt/invent-mgmt.component.ts
+++ src/app/invent-mgmt/invent-mgmt.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 import { InventoryService } from '../services/inventory.service';
 import * as moment from 'moment';
 
@@ -8,6 +8,7 @@ import * as moment from 'moment';
   styleUrls: ['./invent-mgmt.component.css']
 })
 export class InventMgmtComponent implements OnInit {
+  @ViewChild('myForm',{static: false}) formValues;
   item = {};
 
   constructor(private invent: InventoryService) { }
@@ -20,7 +21,9 @@ export class InventMgmtComponent implements OnInit {
     this.item["id"] = Math.floor(Math.random() * 100001) + 1000;
     this.item["created"] = moment().format("MM-DD-YYYY HH:mm");
     console.log(this.item);
-    this.invent.addItem (this.item).subscribe(data => {console.log(data); });
+    this.invent.addItem (this.item).subscribe(data => {console.log(data); 
+      this.formValues.resetForm();
+    });
   }
 
 }
--- src/app/list-invent/list-invent.component.ts
+++ src/app/list-invent/list-invent.component.ts
@@ -12,7 +12,8 @@ export class ListInventComponent implements OnInit {
   items: any = [];
 
   // injecting the service  dependency into class through constructor
-  constructor(private invent: InventoryService) {}
+  constructor(private invent: InventoryService) { }
+
   // component OnInit lifecycle hook
   ngOnInit() {
     // load the item list during the initialization of the component
@@ -20,6 +21,7 @@ export class ListInventComponent implements OnInit {
       this.items = data;
     });
   }
+
   // function to add item to sell list
   appendItem(id, item, price) {
     let new_entry = true;
@@ -30,36 +32,48 @@ export class ListInventComponent implements OnInit {
         new_entry = false;
       }
     });
+
     // else if item is new in the list, add the item entry to the list
     if (new_entry) {
       this.sell_items.push({ "id": id, "item": item, "price": price, "quantity": 1 });
     }
     this.total += price;
   }
+
   //  function to clear the sell item list if order is cancelled
   cancelOrder() {
     this.sell_items.splice(0, this.sell_items.length);
   }
+
   //  function to update the item in the sercer database after the sell.
   placeOrder() {
     console.log(this.sell_items);
     this.invent.updateItems(this.sell_items).subscribe();
     this.sell_items.splice(0, this.sell_items.length);
 
-    setTimeout(() => { this.refresh(); }, 10);
+    setTimeout(() => { this.refresh(); }, 100);
   }
+
   // call this function to get the fresh item lsit
   refresh() {
     this.invent.list_all().subscribe(data => {
       this.items = data;
     });
   }
+
   // function to increament quantity by 1.
   increment(id) {
-    console.log(id);
+    let available;
+    // get total available quantity of the particular item
+    this.items.forEach(element => {
+      if (element.id === id) {
+        available = element.total_count;
+      }
+    });
     // iterate through the list and update the item based on ID
     this.sell_items.forEach(element => {
-      if (element.id === id) {
+      // increament the item only if the quantity is available and Id match is found 
+      if (element.id === id && element.quantity < available) {
         element.quantity += 1;
         this.total += element.price;
       }
